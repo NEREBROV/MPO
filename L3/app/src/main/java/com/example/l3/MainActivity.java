a836a77556d13897cfd4a8d4435448bbfc38dd44
Rebrov Nikita <riebrovn2000@gmail.com>
2020-11-19 00:27:28 +0700

    сэндвич подан

--- L3/app/src/main/java/com/example/l3/MainActivity.java
+++ L3/app/src/main/java/com/example/l3/MainActivity.java
@@ -2,20 +2,12 @@ package com.example.l3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ContentFiller content = new ContentFiller();
-    private Adapter_Regular rViewAdapter = new Adapter_Regular(this, content.ITEMS);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RecyclerView rView = findViewById(R.id.list);
-        rView.setLayoutManager(new LinearLayoutManager(this));
-        rView.setAdapter(rViewAdapter);
     }
 }
